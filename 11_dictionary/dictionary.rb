8c66a724075d9a0014084182630d62f0c77a7b82
Chirag Aggarwal <chi6rag@gmail.com>
2015-09-17 17:25:07 +0530

    11_dictionary - Created find method - #find: doesn't find a word in empty dictionary - #find: finds nothing if the prefix matches nothing

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -11,6 +11,11 @@ class Dictionary
 	def include?(elem)
 		keywords.include? elem
 	end
+	def find(elem)
+		return {} if @entries[elem].nil?
+		@entries[elem]
+	end	
+
 	def add(elem)
 		elem.class == Hash ? @entries.merge!(elem) : @entries.store(elem, nil)
 	end
