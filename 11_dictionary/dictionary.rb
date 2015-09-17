c41881f7bdc9666caaa925008876a4e583e778ad
Chirag Aggarwal <chi6rag@gmail.com>
2015-09-17 17:20:39 +0530

    11_dictionary | Created include? method

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -8,7 +8,9 @@ class Dictionary
 	def keywords
 		@entries.keys.sort
 	end
-
+	def include?(elem)
+		keywords.include? elem
+	end
 	def add(elem)
 		elem.class == Hash ? @entries.merge!(elem) : @entries.store(elem, nil)
 	end
