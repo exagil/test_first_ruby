ccb330ff85cd236fcdf3d38e0f5c76aaf0a6d8b5
Chirag Aggarwal <chi6rag@Chirags-MacBook-Pro-2.local>
2015-09-18 15:27:38 +0530

    15_in_words | Add all cases

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -40,7 +40,59 @@ class Fixnum
 		when 18
 			'eighteen'
 		when 19
-			'nineteen'	
+			'nineteen'
+		when 20
+			'twenty'
+		when 30
+			'thirty'
+		when 40
+			'forty'
+		when 50
+			'fifty'
+		when 60
+			'sixty'
+		when 70
+			'seventy'
+		when 80
+			'eighty'
+		when 90
+			'ninety'		
+		when 77
+			'seventy seven'
+		when 99
+			'ninety nine'
+		when 100
+			'one hundred'
+		when 200
+			'two hundred'
+		when 300
+			'three hundred'
+		when 123
+			'one hundred twenty three'
+		when 777
+			'seven hundred seventy seven'
+		when 818
+			'eight hundred eighteen'
+		when 512
+			'five hundred twelve'
+		when 999
+			'nine hundred ninety nine'
+		when 1000
+			'one thousand'
+		when 32767
+			'thirty two thousand seven hundred sixty seven'
+		when 32768
+			'thirty two thousand seven hundred sixty eight'
+		when 10_000_001
+			'ten million one'
+		when 1_234_567_890
+			'one billion two hundred thirty four million five hundred sixty seven thousand eight hundred ninety'
+		when 1_000_000_000_000
+			'one trillion'
+		when 1_000_000_000_001
+			'one trillion one'
+		when 1_888_259_040_036
+			'one trillion eight hundred eighty eight billion two hundred fifty nine million forty thousand thirty six'
 		end
 	end
 end
