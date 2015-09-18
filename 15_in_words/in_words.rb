689b07098c5f1795ccae92f642bc6ba88af9e571
Chirag Aggarwal <chi6rag@Chirags-MacBook-Pro-2.local>
2015-09-18 15:16:06 +0530

    15_in_words | Created #in_words

---
+++ 15_in_words/in_words.rb
@@ -0,0 +1,26 @@
+class Fixnum
+	def in_words
+		case self
+		when 0
+			'zero'
+		when 1
+			'one'
+		when 2
+			'two'
+		when 3
+			'three'
+		when 4
+			'four'
+		when 5
+			'five'
+		when 6
+			'six'
+		when 7
+			'seven'
+		when 8
+			'eight'
+		when 9
+			'nine'
+		end
+	end
+end
