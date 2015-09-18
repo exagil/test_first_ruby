1299e1d9d09422ded5348637e0b9596f9e73a5c9
Chirag Aggarwal <chi6rag@Chirags-MacBook-Pro-2.local>
2015-09-18 13:03:36 +0530

    13_xml_document | modifies #hello to render a block with text inside

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,9 +1,13 @@
 class XmlDocument
-	def hello(name: nil)
+	def hello(name: nil,&block)
 		if name
 			"<hello name='#{name}'/>"
 		else
+			if block
+			"<hello>#{block.call}</hello>" 
+			else
 			"<hello/>"
+		end
 		end
 	end
 
