4146c64a4d1b094da083f8ff49affc2414e59367
Chirag Aggarwal <chi6rag@Chirags-MacBook-Pro-2.local>
2015-09-18 14:41:01 +0530

    13_xml_document | Add indentation

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,13 +1,19 @@
 class XmlDocument
+	def initialize(indents=false)
+		@indents = indents
+		@index_level = 0
+	end
+	
 	def method_missing(name, **args, &block)
 		if block
-			"<#{name}>#{ block.call }</#{name}>"
+			"<#{name}>#{"\n"+' '*@index_level+=2 if @indents}#{ block.call }</#{name}>#{"\n"+' '*(@index_level >0 ? @index_level-=2 : @index_level) if @indents}"
 		else
 			if args.any?
-				"<#{name} #{args.keys[0]}='#{args[args.keys[0]]}'/>"
+				"<#{name} #{args.keys[0]}='#{args[args.keys[0]]}'/>#{"\n"+' '*(@index_level-=2)  if @indents}"
 			else
-				"<#{name}/>"
+				"<#{name}/>#{"\n"if @indents}"
 			end
 		end
 	end
+
 end
